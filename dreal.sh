ff6277515fd331421d661571d1ff3d41f07f9375
Martin Sidaway <msidaway@gmail.com>
2020-07-12 11:24:27 +0100

    dreal.sh: Option --gdb to run dreal under gdb

--- dreal.sh
+++ dreal.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 here="$(dirname "$0")"
+maybe_gdb=
+
+if [ "$1" = "--gdb" ]; then
+    maybe_gdb="gdb --args"
+    shift
+fi
 
 export LD_LIBRARY_PATH="$here"/qsopt-ex/lib:$LD_LIBRARY_PATH
 
-"$here"/bazel-bin/dreal/dreal "$@"
+$maybe_gdb "$here"/bazel-bin/dreal/dreal "$@"
