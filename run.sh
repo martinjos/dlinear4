ab59943d16dbc3d66c92d57b957f4032a4433506
Martin Sidaway <msidaway@gmail.com>
2020-10-01 13:30:35 +0100

    submodules: Update qsopt_ex
    * Also adapted run.sh so it can use gdb.

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 here="$(dirname "$0")"
+maybe_gdb=
+
+if [ "$1" = "--gdb" ]; then
+    maybe_gdb="gdb --args"
+    shift
+fi
 
 export LD_LIBRARY_PATH="$here"/install/qsopt-ex/lib:$LD_LIBRARY_PATH
 
-"$@"
+$maybe_gdb "$@"
